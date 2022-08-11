04ccf3a55eaa7bd6123d6335c4b253c0e86a391e
PatrycjaJakubow <106872766+PatrycjaJakubow@users.noreply.github.com>
2022-08-11 11:10:53 +0200

    initial upload

---
+++ index.js
@@ -0,0 +1,9 @@
+var pageViews = ["10K", "50K", "100K", "500K", "1M"];
+var prices = ["8", "12", "16", "24", "36"];
+
+
+
+var slide = $("#pricing-range");
+slide.onchange = function() {
+    console.log(this.value);
+}
